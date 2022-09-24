e7f9e3becf11fd91c045edc91dd614f25765126d
1charl <105971215+1charl@users.noreply.github.com>
2022-09-24 09:56:01 +0100

    Update PlayerArcher.js

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -8,12 +8,12 @@ class PlayerArcher {
   //  this.body = Matter.Bodies(x, y, width, height, options);
   //  this.body = Matter.Bodies.rect(x, y, width, height);
   //  this.body = Matter.rectangle(x, y, width, height, options);
-  //  this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+  this.body = Matter.Bodies.rectangle(x, y, width, height, options);
 
     this.width = width;
     this.height = height;
     this.collapse = false;
-    this.image = loadImage("./assets/playerArcher.png");
+    this.image = loadImage("assets/playerArcher.png");
 
     World.add(world, this.body);
     Matter.Body.setAngle(this.body, -90);
@@ -31,6 +31,10 @@ class PlayerArcher {
     pop();
   }
 }
+
+
+
+
 
 
 
